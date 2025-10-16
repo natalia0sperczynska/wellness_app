e4c77a593df7254d1651a7d9ec8f1e93f3c2e22e
nsperczynska <sperczynskanatalia@gmail.com>
2025-10-16 23:11:17 +0200

    dashboard page widget refactorization

--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -1,38 +1,48 @@
 import 'package:wellness_app/commons.dart';
+
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final textTheme =Theme.of(context).textTheme;
     return ChangeNotifierProvider(
-      create: (_) => DashboardPageController(),
-      child: Consumer<DashboardPageController>(
-        builder: (context, controller, _) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Your wellness score for today '),
-              centerTitle: true,
-            ),
-            body: Padding(
-              padding: const EdgeInsets.all(AppSizes.padding),
-              child: SingleChildScrollView(
-                child:Column(
-                  children: [
-                    const GreetingHeader(),
-                    const SizedBox(height: 20),
-                  ScoreCircle(score: controller.wellnessScore),
-                    const SizedBox(height:20),
-                    ScoreBreakdown(stepsScore: 8500, moodScore: 4, habitsScore: 3, hydrationScore: 6),
-                  const SizedBox(height: 20),
-                ]
-            ),
-            )
-          )
-          );
-        },
-      ),
+        create: (_) => DashboardPageController(),
+        child: Consumer<DashboardPageController>(
+            child: const _StaticDashboardHeader(),
+            builder: (context, controller, StaticDashboardHeader) {
+              return Scaffold(
+                appBar: AppBar(
+                  // Używamy tytułu z motywu
+                  title: const Text('Your Wellness Score'),
+                ),
+                body:SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(AppSizes.gapMedium),
+                    child: Column(
+                        children: [
+                      StaticDashboardHeader!,
+                const SizedBox(height: 30),
+                ScoreCircle(score: controller.wellnessScore),
+                const SizedBox(height: 20),
+                ScoreBreakdown(stepsScore: 8500,
+                    moodScore: 4,
+                    habitsScore: 3,
+                    hydrationScore: 6)
+                    ],
+                  ),
+                ),
+              ),
+              );
+            },
+        ),
     );
   }
 }
+
+class _StaticDashboardHeader extends StatelessWidget {
+  const _StaticDashboardHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [ GreetingHeader()]);}}
